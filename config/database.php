dccb108f7e926d50759342ae36460b2bc9f8a6a5
unknown <xiaohuaaa06@gmail.com>
2026-09-10 15:45:31 +0800

    tb

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ define('DB_NAME', 'synergy1_lawlifang_tb');
 define('DB_USER', 'synergy1_yenping');
 define('DB_PASS', 'R.zb0ZwEuGZ}*fW2');
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
